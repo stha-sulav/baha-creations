e3bff7402e8a4ee31f9b6b5ca5de47f515c060a8
Sulav <sulavshrestha2323@gmail.com>
2026-05-12 05:12:01 +0000

    Initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/feat/shared/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Baha Creations
+      <Button>Tests</Button>
+    </div>
+  );
+}
